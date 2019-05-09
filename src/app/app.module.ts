083abe2a54af4d99c6d6a245d7ffb50b2b844f89
WilliamHXu <william.haofei.xu@gmail.com>
2019-05-09 09:54:25 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent
   ],
   imports: [
     BrowserModule,
